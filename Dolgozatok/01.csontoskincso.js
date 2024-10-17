ad34c756ae16f0a571038d818733f2239e76dccd
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2024-10-17 10:56:30 +0200

    02.Feladat
    02.Feladat

--- Dolgozatok/01.csontoskincso.js
+++ Dolgozatok/01.csontoskincso.js
@@ -1,15 +1,4 @@
-/*
-
-    01.Feladat:
-        A függvény egy számlistát kezeljen. Minden szám legyen megszorozva a saját indexével. A megoldás ezen szorzatok összegennnnnnnnnnnnnnnnnnnnn legyen.
-        Pl.: console.log(indexszoroz[1, 2, 3, 4, 5]) -> 40 lesz a megoldás (mert 1*0 + 2*1 +  3*2 + 4*3 + 5*4 = 40)
-        
-        Tippek: segédváltozó az összegnek, for ciklus, indexkezelés, +=
-
-        -------------------------------------
-
-*/
-
+console.log("01.Feladat:")
 function indexszoroz(szamlista) {
     let osszeg = 0;
     for (let i = 0; i < szamlista.length; i++) {
@@ -19,3 +8,17 @@ function indexszoroz(szamlista) {
 }
 
 console.log(indexszoroz([1, 2, 3, 4, 5]));
+
+console.log("02.Feladat:")
+
+function parosIndexEgybeesik(szamlista) {
+    for (let i = 0; i < szamlista.length; i++) {
+        if ((szamlista[i] % 2) !== (i % 2)) {
+            return false;
+        }
+    }
+    return true; 
+}
+
+console.log(parosIndexEgybeesik([2, 7, 4, 9, 6, 1, 6, 3]));
+console.log(parosIndexEgybeesik([2, 7, 9, 1, 6, 1, 6]));
