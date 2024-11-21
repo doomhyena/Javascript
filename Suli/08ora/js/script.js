f9e69cdf24eb4294fcbccd128aab98f2769df6c4
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2024-11-21 10:33:07 +0100

    Tartalmak beolvasásása
    Definiáltuk majd beolvastuk a tartalmat

---
+++ Suli/08ora/js/script.js
@@ -0,0 +1,5 @@
+const kezdgomb = document.getElementById('start-btn');
+const kovektkezogomb = document.getElementById('next-btn');
+const kerdestarto = document.getElementById('kerdes-container');
+const kerdeselem = document.getElementById('kerdes');
+const valaszgombelem = document.getElementById('egyediValasz-buttons');
