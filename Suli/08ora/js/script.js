1a7ddaa5d42ea87d0df5ebfb78a8c70f2c3cc52e
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2024-11-21 10:38:38 +0100

    e
    - válaszgombelem definiálása
    - megkevertkerdesek, jelenlegikerdesindex definiálása
    - esemény létrehozása

--- Suli/08ora/js/script.js
+++ Suli/08ora/js/script.js
@@ -3,3 +3,7 @@ const kovektkezogomb = document.getElementById('next-btn');
 const kerdestarto = document.getElementById('kerdes-container');
 const kerdeselem = document.getElementById('kerdes');
 const valaszgombelem = document.getElementById('egyediValasz-buttons');
+
+let megkevertkerdesek, jelenlegikerdesindex
+
+kezdgomb.addEventListener('click', jatekkezdes);
